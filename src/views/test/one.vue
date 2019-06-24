5b6aea6fb0b406d81885693fc273af86a6756ce3
16060011 <lixiaocheng@shinho.net.cn>
2019-06-24 16:54:08 +0800

    first commit
    first commit

---
+++ src/views/test/one.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-container">
+    test-one
+  </div>
+</template>
+<script>
+export default {
+  name: 'One',
+  components: { },
+  data() {
+    return {
+
+    }
+  },
+  mounted: function() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+</style>
---
+++ src/views/test/two.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-container">
+    test-two
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'Two',
+  components: { },
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  mounted: function() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+</style>

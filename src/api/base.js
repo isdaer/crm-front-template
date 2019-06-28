f4f0de99b7ba3bbac38ba6a527513818540fadbc
16060011 <lixiaocheng@shinho.net.cn>
2019-06-28 14:52:47 +0800

    调整

---
+++ src/api/base.js
@@ -0,0 +1,35 @@
+/**
+ * 接口域名的管理
+ */
+let base = {}
+const BASE_HOST = 'https://apimarket-dev.shinho.net.cn'
+const BASE_PROD_HOST = 'https://sso-dev.shinho.net.cn'
+// const BASE_HOST = 'https://sso-dev.shinho.net.cn/'
+const base_DEV = {
+  BPM_URL: `https://eccp-workflow-dev.shinho.net.cn/eccp-workflow`,
+  PORTAL_URL: `${BASE_HOST}/eccp-portal`, // eccp门户
+  ATTENDANCE_URL: `${BASE_HOST}/eccp-bpmkql`, // eccp考勤类
+  // TEST: 'http://10.10.1.134:8080/eccp-bpmkql',
+  DOWNLOAD_URL: `${BASE_PROD_HOST}/eccp-bpmkql`
+}
+const base_TEST = {
+  BPM_URL: `https://eccp-workflow-dev.shinho.net.cn/eccp-workflow`,
+  PORTAL_URL: `${BASE_HOST}/eccp-portal`, // eccp门户
+  ATTENDANCE_URL: `${BASE_HOST}/eccp-bpmkql`, // eccp考勤类
+  DOWNLOAD_URL: `${BASE_PROD_HOST}/eccp-bpmkql`
+}
+const base_PROD = {
+  BPM_URL: `https://eccp-workflow-dev.shinho.net.cn/eccp-workflow`,
+  PORTAL_URL: `${BASE_PROD_HOST}/eccp-portal`, // eccp门户
+  ATTENDANCE_URL: `${BASE_HOST}/eccp-bpmkql`, // eccp考勤类
+  DOWNLOAD_URL: `${BASE_PROD_HOST}/eccp-bpmkql`
+}
+if (process.env.NODE_ENV === 'development') {
+  base = base_DEV
+} else if (process.env.NODE_ENV === 'test') {
+  base = base_TEST
+} else if (process.env.NODE_ENV === 'production') {
+  base = base_PROD
+}
+
+export default base

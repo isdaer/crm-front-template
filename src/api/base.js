ad3f93d70962b4c8212807d7d0c0ce47563535fa
16060011 <lixiaocheng@shinho.net.cn>
2019-06-28 17:24:35 +0800

    删除整理

--- src/api/base.js
+++ src/api/base.js
@@ -4,12 +4,10 @@
 let base = {}
 const BASE_HOST = 'https://apimarket-dev.shinho.net.cn'
 const BASE_PROD_HOST = 'https://sso-dev.shinho.net.cn'
-// const BASE_HOST = 'https://sso-dev.shinho.net.cn/'
 const base_DEV = {
   BPM_URL: `https://eccp-workflow-dev.shinho.net.cn/eccp-workflow`,
   PORTAL_URL: `${BASE_HOST}/eccp-portal`, // eccp门户
   ATTENDANCE_URL: `${BASE_HOST}/eccp-bpmkql`, // eccp考勤类
-  // TEST: 'http://10.10.1.134:8080/eccp-bpmkql',
   DOWNLOAD_URL: `${BASE_PROD_HOST}/eccp-bpmkql`
 }
 const base_TEST = {
